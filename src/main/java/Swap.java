2f3b8dc4e105e7d7aa87927ff81dd04ad3dfd980
Magnamags <151983433+Magnamags@users.noreply.github.com>
2024-09-12 11:32:48 -0400

    First Program added

---
+++ src/main/java/Swap.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Swap {
+    public static void main(String[] args) {
+        System.out.println("Enter the values of a and b : ");
+        Scanner Sc = new Scanner(System.in);
+        int a = Sc.nextInt();
+        int b = Sc.nextInt();
+        System.out.println("Values before swapping: " +a+ " " +b);
+        a=a+b;
+        b=a-b;
+        a=a-b;
+        System.out.println("Values After swapping: " +a+ " "+b);
+    }
+}
